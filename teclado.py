ab92ff41d6dc2ff6ef3dd1e041fdc2a79668f7bb
unknown <ingelsavi@gmail.com>
2021-02-10 14:35:41 -0500

    Clase teclado lista

---
+++ teclado.py
@@ -0,0 +1,20 @@
+from dispositivos_entrada import DispositivosEntrada
+
+class Teclado(DispositivosEntrada):
+    contador_teclados = 0
+    def __init__(self,tipo, marca):
+        Teclado.contador_teclados += 1
+        self.__id = Teclado.contador_teclados
+        self._marca = marca
+        self._tipo = tipo
+        
+    def __str__(self):
+        return(
+            f"ID: {self.__id},  "
+            f"Marca: {self._marca}, "
+            f"Tipo: {self._tipo}"
+        )
+        
+t1 = Teclado("Gamer","Razer")
+print(t1)
+    
